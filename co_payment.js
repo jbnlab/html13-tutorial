18f256cfe31daf30050adc68867f05cb8a1b42b8
James <jbnguyen7@yahoo.com>
2025-03-20 13:37:25 -0400

    Session 13.1 & 13.2

---
+++ co_payment.js
@@ -0,0 +1,64 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
+   Tutorial 13
+   Tutorial Case
+
+   Payment Form Script
+   
+   Author: James-Bond Nguyen
+   Date:   3/18/2025
+   
+   Filename: co_payment.js
+   
+   Function List
+   =============
+   
+   runSubmit()
+      Runs validation tests when the submit button is clicked
+      
+   validateCVC()
+      Validates the credit card CVC number
+      
+   validateMonth()
+      Validates that the user has selected the expiration month of the credit card
+      
+   validateYear()
+      Validates that the user has selected the expiration year of the credit card
+      
+   validateNumber()
+      Validates that the user has entered a valid and legitimate card number
+      
+   validateCredit()
+      Validates that the user has selected a credit card type
+      
+   validateName()
+      Validates that the user has specified the name on the credit card
+      
+   sumDigits(numStr)
+      Sums the digits characters in a text string
+      
+   luhn(idNum)
+      Returns true of idNum satisfies the Luhn Algorithm
+
+*/
+
+window.addEventListener("load", function() {
+   // Retrieve the field/value pairs from the URL
+   var formData = location.search.slice(1);
+   formData = formData.replace(/\+/g," ");
+   formData = decodeURIComponent(formData);
+   var formFields = formData.split(/[&=]/g);
+
+   // Write the field values to the order form
+   document.forms.order.elements.orderDate.value = formFields[1];
+   document.forms.order.elements.modelName.value = formFields[5];
+   document.forms.order.elements.qty.value = formFields[7];
+   document.forms.order.elements.initialCost.value = formFields[9];
+   document.forms.order.elements.protectionName.value = formFields[13];
+   document.forms.order.elements.protectionCost.value = formFields[15];
+   document.forms.order.elements.subtotal.value = formFields[17];
+   document.forms.order.elements.salesTax.value = formFields[19];
+   document.forms.order.elements.totalCost.value = formFields[21];
+});
